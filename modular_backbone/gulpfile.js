6ad8aca3e6c74d6d39777c06f6f5029a5916355b
Didier Berlioz <berliozd@gmail.com>
2015-10-03 01:14:25 +0200

    add scss, ugify, minify and cleaning tasks

--- modular_backbone/gulpfile.js
+++ modular_backbone/gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var stringify = require('stringify');
+var sass = require('gulp-sass');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+
 
 // Basic usage
-gulp.task('browserify', function () {
+gulp.task('browserify', ['clean-js'], function () {
   // Single entry point to browserify
   gulp.src('js/main.js')
     .pipe(browserify({
@@ -13,12 +18,32 @@ gulp.task('browserify', function () {
         minify: true
       })
     }))
+    .pipe(uglify())
     .pipe(gulp.dest('./build/js'))
 });
 
-gulp.task('default', ['browserify']);
-
-gulp.task('watch', function () {
-  gulp.watch('js/**/*.js', ['default']);
+gulp.task('scss', ['clean-css'], function() {
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass())
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./build/css'))
 });
 
+gulp.task('clean-css', function () {
+  return del([
+    'build/css/**/*.css',
+  ]);
+});
+
+gulp.task('clean-js', function () {
+  return del([
+    'build/js/**/*.js',
+  ]);
+});
+
+gulp.task('watch', function () {
+  gulp.watch('js/**/*.js', ['browserify']);
+  gulp.watch('scss/**/*.scss', ['scss']);
+});
+
+gulp.task('default', ['browserify', 'scss', 'watch']);
